88d2985ef68e86b2953c9a68cc5038740db1f1a9
Starson182 <ericzh1616@gmail.com>
2019-02-23 18:09:17 -0500

    Update Google Search.py

--- Google Search.py
+++ Google Search.py
@@ -1,5 +1,13 @@
-class search():
-    def __init__(self):
-        pass
+import webbrowser
 
-        
+tabUrl = "http://google.com/?#q"
+term = input("Enter search query: ")
+
+webbrowser.open(tabUrl + term, new = True)
+
+'''
+class search():
+    ingredients = ['egg', 'bacon', 'ham', 'bread']
+    def __init__(self):
+
+'''
