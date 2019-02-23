2365767fee9d4466ca65e9f31e43eff3be2172ec
Starson182 <ericzh1616@gmail.com>
2019-02-23 18:49:12 -0500

    Update Google Search.py

--- Google Search.py
+++ Google Search.py
@@ -1,26 +1,19 @@
 import webbrowser
 tabUrl = "https://www.google.com/search?q="
-totalSearch = []
+searches = []
 
 class search():
     ingredients = 'egg, tomato, cheese, potato'
-    array = ingredients.split(',')
+    array = ingredients.split(', ')
     
     for i in range(len(array)):
-        #webbrowser.open(tabUrl + array[i] + ' recipes', new = True)
-        totalSearch.append(array[i])
         temp = ''
-        print('loading')
 
         for j in range(len(array)):
-            if j <= i:
+            if j >= i:
                 temp += array[j] + ', '
-                print('loading..')
-                
-                for k in totalSearch:
-                    if temp != k:
-                        totalSearch.append(array[j])
-                        print('loading....')
-                        break
+                searches.append(temp)
 
-print(totalSearch)
+    for i in searches:
+        webbrowser.open(tabUrl + i + 'recipes', new = True)
+    
