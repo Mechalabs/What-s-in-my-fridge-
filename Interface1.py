abeb088a369271d25019c3e2b17da23ca7f7961b
supermule <45317559+supermule@users.noreply.github.com>
2019-03-23 17:45:15 -0400

    Add files via upload

---
+++ Interface1.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+
+root.title = ("GUI")
+font1 = "Courier"
+
+title = Label(root, text = "What's in your fridge?", font=(font1, 36))
+title.pack()
+prompt = Label (root, text = "Enter your ingredients separated by a comma:", font = (font1, 12))
+prompt.pack()
+entry = Entry(root, width = 50)
+entry.pack()
+entry.focus_set()
+def callback(): 
+    ingredients = (entry.get())
+    arr = [ingredients.strip().split(',')]
+Enterbtn = Button(text = "Enter", command = callback)
+Enterbtn.pack()
+root.mainloop() 
